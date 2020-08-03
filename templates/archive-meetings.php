6b11c109c4782eabd6f9eede270c94bdbc593d97
Deepen Bajracharya <dpen.connectify@gmail.com>
2020-08-03 18:07:54 +0545

    Archive template change

--- templates/archive-meetings.php
+++ templates/archive-meetings.php
@@ -11,22 +11,37 @@
 defined( 'ABSPATH' ) || exit;
 
 get_header();
+
+/**
+ * Before the LOOP archive page
+ */
+do_action( 'vczapi_before_main_archive_content' );
 ?>
 
-    <div id="dpn-zvc-primary" class="dpn-zvc-primary container">
+    <div id="vczapi-primary" class="vczapi-primary container">
+        <div class="vczapi-list-zoom-meetings">
+            <div class="vczapi-list-zoom-meetings--items">
+				<?php if ( have_posts() ) {
+					// Start the Loop.
+					while ( have_posts() ) {
+						the_post();
 
-		<?php if ( have_posts() ) {
-			// Start the Loop.
-			while ( have_posts() ) {
-				the_post();
+						do_action( 'vczapi_main_archive_loop' );
 
-				vczapi_get_template_part( 'content', 'meeting' );
-			}
-		} else {
-			echo "<p>" . __( 'No Meetings found.', 'video-conferencing-with-zoom-api' ) . "</p>";
-		}
-		?>
+						vczapi_get_template_part( 'content', 'meeting' );
+					}
+				} else {
+					echo "<p>" . __( 'No Meetings found.', 'video-conferencing-with-zoom-api' ) . "</p>";
+				}
+				?>
+            </div>
+        </div>
     </div>
 
 <?php
+/**
+ * After loop call
+ */
+do_action( 'vczapi_after_main_archive_content' );
+
 get_footer();
--- templates/content-meeting.php
+++ templates/content-meeting.php
@@ -10,8 +10,28 @@
 
 defined( 'ABSPATH' ) || exit;
 
+global $zoom;
 ?>
-<div id="dpn-zvc-<?php the_ID(); ?>" class="dpn-zvc-<?php the_ID(); ?>">
-    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-	<?php the_excerpt(); ?>
-</div>
+<div class="vczapi-list-zoom-meetings--item">
+	<?php if ( has_post_thumbnail() ) { ?>
+        <div class="vczapi-list-zoom-meetings--item__image">
+			<?php the_post_thumbnail(); ?>
+        </div><!--Image End-->
+	<?php } ?>
+    <div class="vczapi-list-zoom-meetings--item__details">
+        <h3><?php the_title(); ?></h3>
+        <div class="vczapi-list-zoom-meetings--item__details__meta">
+            <div class="hosted-by meta">
+                <strong><?php _e( 'Hosted By:', 'video-conferencing-with-zoom-api' ); ?></strong> <span><?php echo get_the_author(); ?></span>
+            </div>
+            <div class="start-date meta">
+                <strong><?php _e( 'Start', 'video-conferencing-with-zoom-api' ); ?>:</strong>
+                <span><?php echo vczapi_dateConverter( $zoom['api']->start_time, $zoom['api']->timezone, 'F j, Y @ g:i a' ); ?></span>
+            </div>
+            <div class="timezone meta">
+                <strong><?php _e( 'Timezone', 'video-conferencing-with-zoom-api' ); ?>:</strong> <span><?php echo $zoom['api']->timezone; ?></span>
+            </div>
+        </div>
+        <a href="<?php echo esc_url( get_the_permalink() ) ?>" class="btn"><?php _e( 'See More', 'video-conferencing-with-zoom-api' ); ?></a>
+    </div><!--Details end-->
+</div><!--List item end-->
